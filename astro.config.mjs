dc7dd258075d6a825c97e7f3acee1224bb68512c
Dipankar Maikap <dipankarmaikap77@gmail.com>
2025-06-18 03:30:30 +0530

    feat: vercel adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercelServerless from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  output: "server",
+  adapter: vercelServerless({}),
 });
